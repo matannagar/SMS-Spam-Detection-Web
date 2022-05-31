34622f531c66d6ceee5473d710c1c860b3e88a3f
matannagar <73398186+matannagar@users.noreply.github.com>
2022-05-31 17:04:39 +0300

    last update

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 import torch
-from transformers import RobertaTokenizer
+
 
 from transformers import RobertaTokenizer
 
@@ -8,34 +8,28 @@ MODEL_PATH = "./outputs"
 
 
 def prediction(sms):
-    model_args = ClassificationArgs(num_train_epochs=1, use_multiprocessing=False,
-                                    use_multiprocessing_for_evaluation=False, process_count=1)
-    model = ClassificationModel(
-        "roberta", "./outputs",
-        args=model_args, use_cuda=False)
     tokenizer = RobertaTokenizer.from_pretrained(
         "./outputs")
 
-    model_args = ClassificationArgs(num_train_epochs=1)
-    #
-    # # Create a ClassificationModel
+    # checks if GPU is available
     cuda_available = torch.cuda.is_available()
-    # cuda_available = False
 
-    # Create a ClassificationModel
-    # this loads the models from the output directory
     print("starting to load model")
+    # model args for creation
+    model_args = ClassificationArgs(num_train_epochs=1)
+
+    # this loads the models from the output directory
     model = ClassificationModel(
         "roberta", MODEL_PATH, args=model_args,
         use_cuda=cuda_available
     )
     print("performing prediction")
-    # predictions, raw_outputs = model.predict([sms])
+    inputs = tokenizer(sms, return_tensors="pt")
+
+    outputs = model.model(**inputs)
     print("returning prediction")
 
-    inputs = tokenizer(sms, return_tensors="pt")
-    outputs = model.model(**inputs)
-
+    # make pretty
     if outputs[0][0][0].item() > outputs[0][0][1].item():
         return "Non-Spam"
     else:
--- train_model/training.py
+++ train_model/training.py
@@ -2,37 +2,42 @@ from sklearn.model_selection import train_test_split
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 import pandas as pd
 import logging
+import torch
 
+if __name__ == '__main__':
+    #  Transformers has a centralized logging system, so that you can setup the verbosity of the library easily.
+    logging.basicConfig(level=logging.INFO)
+    transformers_logger = logging.getLogger("transformers")
+    transformers_logger.setLevel(logging.WARNING)
 
-logging.basicConfig(level=logging.INFO)
-transformers_logger = logging.getLogger("transformers")
-transformers_logger.setLevel(logging.WARNING)
+    # Organize Data Frame
+    df = pd.read_csv("spam.csv", index_col=False, header=None, encoding='latin-1')
 
-# Organize Data Frame
-df = pd.read_csv("spam.csv", encoding='latin-1')
+    # change label of headers
 
-df.drop(df.columns[df.columns.str.contains(
-    'unnamed', case=False)], axis=1, inplace=True)
+    df = df.iloc[:, [1, 0]]
+    df[0] = df[0].map({'ham': 1, 'spam': 0})
 
-df = df[["v1", "v2"]]
+    # remove null data
+    df = df[df[0].notna()]
 
-logging.basicConfig(level=logging.INFO)
-transformers_logger = logging.getLogger("transformers")
-transformers_logger.setLevel(logging.WARNING)
+    # split data to train and test
+    train, test = train_test_split(df, test_size=0.2)
 
-# Optional model configuration
-model_args = ClassificationArgs(num_train_epochs=1)
+    # checks if GPU is available
+    cuda_available = torch.cuda.is_available()
 
-# Create a ClassificationModel
-model = ClassificationModel(
-    "roberta", "roberta-base", args=model_args
-)
+    # Optional model configuration
+    model_args = ClassificationArgs(num_train_epochs=1)
 
-# split data to train and test
-train, test = train_test_split(df, test_size=0.2)
+    # Create a ClassificationModel
+    model = ClassificationModel(
+        "roberta", "roberta-base", args=model_args, use_cuda=cuda_available
+    )
+    df[0] = df[0].astype(int)
+    print(train)
+    # Train the model
+    model.train_model(train)
 
-# Train the model
-model.train_model(train)
-
-# Evaluate the model
-result, model_outputs, wrong_predictions = model.eval_model(test)
+    # Evaluate the model
+    result, model_outputs, wrong_predictions = model.eval_model(test)
